08bf050ed5f9b3263c38466d5ee717bccc5df8dc
albu9304 <alexander.bukk@gmail.com>
2017-09-14 15:10:36 +0200

    Some updates
    dont remember

--- app/src/main/java/com/example/jonas/androidlavagame/GamePanel.java
+++ app/src/main/java/com/example/jonas/androidlavagame/GamePanel.java
@@ -1,34 +1,41 @@
 package com.example.jonas.androidlavagame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.provider.Settings;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.InputStream;
+
 /**
  * Created by Alexander on 2017-07-12.
  */
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
-
-
     private RectPlayer player;
     private Point playerPosition;
+    /*private Bitmap image = BitmapFactory.decodeResource(
+        "res/drawable-nodpi/tile.png"
+    );*/
+    //private Bitmap image = BitmapFactory.decodeFile("res/drawable-nodpi/tile.png");
+    InputStream is = this.getResources().openRawResource(R.raw.tile);
+    Bitmap image = BitmapFactory.decodeStream(is);
+    private boolean isRunning = false;
 
     public GamePanel(Context context) {
         super(context);
-
         getHolder().addCallback(this);
-
         thread = new MainThread(getHolder(), this);
-
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
         playerPosition = new Point(150, 150);
-
         setFocusable(true);
     }
 
@@ -62,9 +69,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
             case MotionEvent.ACTION_MOVE:
                 playerPosition.set((int)event.getX(), (int)event.getY());
         }
-
-
-
         return true;
         //return super.onTouchEvent(event);
     }
@@ -78,7 +82,10 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         super.draw(canvas);
 
         canvas.drawColor(Color.WHITE);
-
+        canvas.drawBitmap(image, 0, 0, null);
+        System.out.println(image.getWidth());
         player.draw(canvas);
     }
+
+
 }
