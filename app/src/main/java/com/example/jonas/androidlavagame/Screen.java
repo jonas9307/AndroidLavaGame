534360f29fb74ef74f326fd945d41cde8521d820
albu9304 <alexander.bukk@gmail.com>
2017-09-25 21:35:18 +0200

    screen gets drawn
    screen gets drawn, however, there is much clipping, low performance and
    something wrong with the steering

--- app/src/main/java/com/example/jonas/androidlavagame/Screen.java
+++ app/src/main/java/com/example/jonas/androidlavagame/Screen.java
@@ -27,43 +27,58 @@ public class Screen {
             1.1 if left of screen, check if on screen
         */
         top = (int)player.getX();
+        int bot = top + height - 1;
         left = (int)player.getY();
+        int right = left + width - 1;
         int nRows = tileMap.length;
         int nCols = tileMap[0].length;
 
-        for (int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                if((i+1)*128 > top && i*128 < top + height){
-                    if((j+1)*128 > left && j*128 < left + width){
-                        // what part of the tile to draw?
-                        int xTile, yTile, widthTile, heightTile ;
+        int xPixels = 0;
+        int yPixels = 0;
+        
+        for (int i = 0; i < nRows; i++) {
 
-                        if(j*128 < left) {
-                            widthTile = left - j*128;
-                            xTile = 128 - widthTile;
-                        } else if((j+1)*128 > left + width) {
-                            widthTile = left + width - j*128;
-                            xTile = left + width - widthTile;
-                        } else {
-                            xTile = 0;
-                            widthTile = 128;
+            int tileTop = i*128;
+            int tileBot = tileTop + 128 - 1;
+
+            if(tileBot > top && tileTop < bot){
+                for(int j = 0; j < nCols; j++) {
+
+                    int tileLeft = j*128;
+                    int tileRight = tileLeft + 128 - 1;
+
+
+                        if(tileRight > left && tileLeft < right){
+
+                            // what part of the tile to draw?
+                            int newTileLeft, newTileTop, newTileWidth, newTileHeight;
+
+                            if(tileLeft < left) {
+                                newTileWidth = left - tileLeft;
+                                newTileLeft = 128 - newTileWidth;
+                            } else if(tileRight > right) {
+                                newTileWidth = right - tileLeft;
+                                newTileLeft = 0;
+                            } else {
+                                newTileLeft = 0;
+                                newTileWidth = 128;
+                            }
+
+                            if(tileTop < top) {
+                                newTileHeight = top - tileTop;
+                                newTileTop = 128 - newTileHeight;
+                            } else if(tileBot > bot) {
+                                newTileHeight = bot - tileTop;
+                                newTileTop = 0;
+                            } else {
+                                newTileTop = 0;
+                                newTileHeight = 128;
+                            }
+
+                            Bitmap tilePart = Bitmap.createBitmap(tileMap[i][j].getImage(), newTileLeft, newTileTop, newTileWidth, newTileHeight);
+                            canvas.drawBitmap(tilePart, tileLeft - left + newTileLeft, tileTop - top + newTileTop, null);
                         }
-
-                        if(i*128 < top) {
-                            heightTile = top - i*128;
-                            yTile = 128 - heightTile;
-                        } else if((i+1)*128 > top + height) {
-                            heightTile = top + height - i*128;
-                            yTile = top + height - heightTile;
-                        } else {
-                            yTile = 0;
-                            heightTile = 128;
-                        }
-
-                        Bitmap tilePart = Bitmap.createBitmap(tileMap[i][j].getImage(), yTile, xTile, heightTile, widthTile);
-                        canvas.drawBitmap(tilePart, left - j*128 + 128 - widthTile, top - i*128 + 128 - heightTile, null);
                     }
-                }
                 //canvas.drawBitmap(tileMap[i][j].getImage(), j*128, i*128, null);
             }
         }
